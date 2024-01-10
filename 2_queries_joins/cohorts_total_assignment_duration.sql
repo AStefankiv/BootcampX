7e33ab64570b9075d2b82db900ed93c53370b90d
AStefankiv <stefankif35@gmail.com>
2024-01-09 21:28:28 -0700

    Added students and total assignment duration

---
+++ 2_queries_joins/cohorts_total_assignment_duration.sql
@@ -0,0 +1,4 @@
+SELECT SUM(assignment_submissions.duration) AS total_duration 
+FROM students JOIN cohorts ON students.cohort_id = cohorts.id 
+JOIN assignment_submissions ON assignment_submissions.student_id = students.id 
+WHERE cohorts.name = 'FEB12';
